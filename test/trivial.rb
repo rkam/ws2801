2285911fd8fb6d156042d8985a2662eb724c44ba
qiaje <qiaje@sailea.local>
2016-06-30 20:07:47 -0700

    Allow extensive test if argv.length > 0

--- test/trivial.rb
+++ test/trivial.rb
@@ -3,20 +3,26 @@
 #
 require_relative '../lib/ws2801.rb'
 
+EXTENSIVE = ARGV.length > 0
+
+WS2801.generate                # generate empty strip (Would happen from alone if you just start setting colors)
+
 WS2801.length 20               # 25 == default
 WS2801.generate                # pre-generate
 
-#COLORS = [ :r, :g, :b ]
-#LEVELS = [ 255, 192, 128, 64, 32, 16, 8, 0 ]
-
-COLORS = [ :g ]
-LEVELS = [ 128, 32, 0 ]
+if EXTENSIVE
+  COLORS = [ :r, :g, :b ]
+  LEVELS = [ 255, 192, 128, 64, 32, 16, 8, 0 ]
+else
+  COLORS = [ :g ]
+  LEVELS = [ 128, 32, 0 ]
+end
 
 COLORS.each do |c|
   LEVELS.each do |l|
-    puts "green level #{l}"
+    puts "#{c} level #{l}"
     WS2801.set :pixel => :all, c => l
-    sleep(0.33)
+    sleep(0.5)
   end
-  sleep(0.5)
+  sleep(0.75)
 end
